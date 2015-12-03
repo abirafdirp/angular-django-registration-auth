ebbbeb3b1428019526692061d2568762a8a6aefd
abirafdirp <abirafdiraditya@gmail.com>
2015-12-03 10:12:41 +0700

    adapting for kompres project

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -3,19 +3,26 @@
 angular.module('angularDjangoRegistrationAuthApp')
   .controller('LoginCtrl', function ($scope, $location, djangoAuth, Validate) {
     $scope.model = {'username':'','password':''};
-  	$scope.complete = false;
+    $scope.complete = false;
+    $scope.show_loading = false;
+
     $scope.login = function(formData){
       $scope.errors = [];
+      $scope.show_loading = true;
       Validate.form_validation(formData,$scope.errors);
       if(!formData.$invalid){
         djangoAuth.login($scope.model.username, $scope.model.password)
-        .then(function(data){
-        	// success case
-        	$location.path("/");
-        },function(data){
-        	// error case
-        	$scope.errors = data;
-        });
+          .then(function(data){
+            // success case
+            $scope.show_loading = false;
+          },function(data){
+            // error case
+            $scope.show_loading = false;
+            $scope.errors = data;
+          });
+      }
+      else {
+        $scope.show_loading = false;
       }
     }
   });
